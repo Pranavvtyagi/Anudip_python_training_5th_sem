03ef5946e116e67b2c897279126261248400f73a
Pranavvtyagi <pranav.tyagi1818@gmail.com>
2026-06-04 13:56:03 +0530

    Add files via upload

---
+++ anudip2.py
@@ -0,0 +1,7 @@
+#A teacher is recoridng attendence of students enter the classsroom. The class strength is 30 students.
+#WAP that counts the number of students entering the classroom and display the attendence count until all 30 students are present
+for i in range(1, 31):
+    input(" Enter when a student enters the classroom: ")
+    print("Student Entered  Attendance Count =", i)
+
+print("All 30 students are present.")
