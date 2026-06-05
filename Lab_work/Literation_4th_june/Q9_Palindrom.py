70acb44a51fc3a831df256674fbdfb924b25df03
Pranav Tyagi <pranav.tyagi1818@gmail.com>
2026-06-05 15:01:11 +0530

    Create Q9_Palindrom.py

---
+++ Lab_work/Literation_4th_june/Q9_Palindrom.py
@@ -0,0 +1,25 @@
+# Program to check Palindrome Number
+
+num = int(input("Enter a number: "))
+
+original = num
+reverse = 0
+
+# Reverse the number
+while num > 0:
+
+    digit = num % 10
+
+    reverse = (reverse * 10) + digit
+
+    num = num // 10
+
+# Display reverse number
+print("Reverse Number =", reverse)
+
+# Check palindrome
+if original == reverse:
+    print("Palindrome Number")
+
+else:
+    print("Not a Palindrome Number")
